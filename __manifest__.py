98fa4dba5196e6f2a6330bb550baa2051af48e8d
Antonio Queb <cardstillo@gmail.com>
2026-03-22 20:49:48 -0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Form Input Borders",
+    "version": "19.0.1.0.0",
+    "category": "Tools",
+    "summary": "Adds visible borders to form inputs and selects",
+    "author": "Alphaqueb Consulting",
+    "license": "LGPL-3",
+    "depends": ["web"],
+    "assets": {
+        "web.assets_backend": [
+            "form_input_borders/static/src/css/form_input_borders.scss",
+        ],
+    },
+    "installable": True,
+    "auto_install": False,
+}
